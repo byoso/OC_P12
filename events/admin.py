176fe2ae51e713a3ea11fcf421bbdfff34d635f6
byoso <emailvinz@gmail.com>
2022-05-25 00:47:55 +0200

    enought with admin, todo: permissions in the api views

--- events/admin.py
+++ events/admin.py
@@ -1,12 +1,34 @@
+from django.shortcuts import get_object_or_404
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import escape, mark_safe
-from django.contrib.auth import get_permission_codename
+from django.contrib.auth import get_permission_codename, get_user_model
+from django.contrib.auth.models import Group
 from .models import (
     Client,
+    EmployeeClient,
     Contract,
+    EmployeeContract,
     Event,
+    EmployeeEvent,
 )
+
+Employee = get_user_model()
+
+
+class InlineClientAssignment(admin.TabularInline):
+    model = EmployeeClient
+    list_display = ['username']
+
+
+class InlineContractAssignment(admin.TabularInline):
+    model = EmployeeContract
+    list_display = ['username']
+
+
+class InlineEventAssignment(admin.TabularInline):
+    model = EmployeeEvent
+    list_display = ['username']
 
 
 class AdminEvent(admin.ModelAdmin):
@@ -17,6 +39,7 @@ class AdminEvent(admin.ModelAdmin):
 
     model = Event
     list_display = ['name', 'show_contract']
+    inlines = [InlineEventAssignment]
 
     def _get_link(self, obj):
         link_string = reverse(
@@ -38,10 +61,10 @@ class InlineEvent(admin.StackedInline):
 
 class AdminContract(admin.ModelAdmin):
     model = Contract
-    list_display = ['client', 'id', 'date_created', 'signed', 'payed']
+    list_display = ['client', 'date_created', 'signed', 'payed']
     list_filter = ['signed', 'payed']
     search_fields = ['client']
-    inlines = [InlineEvent]
+    inlines = [InlineContractAssignment, InlineEvent]
 
 
 class InlineContract(admin.StackedInline):
@@ -55,64 +78,68 @@ class InlineContract(admin.StackedInline):
 
 class AdminClient(admin.ModelAdmin):
     model = Client
-    list_display = ["first_name", "last_name", "phone", "mobile", "is_client"]
+    list_display = [
+        "first_name", "last_name", "phone", "mobile",
+        "is_client",
+        # "assigned"
+        ]
     search_fields = ["first_name", "last_name"]
     list_filter = ["active", "is_client"]
-    inlines = [InlineContract]
+    inlines = [InlineClientAssignment, InlineContract]
 
-    def has_add_permission(self, request):
-        """
-        Return True if the given request has permission to add an object.
-        Can be overridden by the user in subclasses.
-        """
-        opts = self.opts
-        codename = get_permission_codename("add", opts)
-        return request.user.has_perm("%s.%s" % (opts.app_label, codename))
+    # def has_add_permission(self, request):
+    #     """
+    #     Return True if the given request has permission to add an object.
+    #     Can be overridden by the user in subclasses.
+    #     """
+    #     opts = self.opts
+    #     codename = get_permission_codename("add", opts)
+    #     return request.user.has_perm("%s.%s" % (opts.app_label, codename))
 
-    def has_change_permission(self, request, obj=None):
-        """
-        Return True if the given request has permission to change the given
-        Django model instance, the default implementation doesn't examine the
-        `obj` parameter.
-        Can be overridden by the user in subclasses. In such case it should
-        return True if the given request has permission to change the `obj`
-        model instance. If `obj` is None, this should return True if the given
-        request has permission to change *any* object of the given type.
-        """
-        opts = self.opts
-        codename = get_permission_codename("change", opts)
-        return request.user.has_perm("%s.%s" % (opts.app_label, codename))
+    # def has_change_permission(self, request, obj=None):
+    #     """
+    #     Return True if the given request has permission to change the given
+    #     Django model instance, the default implementation doesn't examine the
+    #     `obj` parameter.
+    #     Can be overridden by the user in subclasses. In such case it should
+    #     return True if the given request has permission to change the `obj`
+    #     model instance. If `obj` is None, this should return True if the given
+    #     request has permission to change *any* object of the given type.
+    #     """
+    #     opts = self.opts
+    #     codename = get_permission_codename("change", opts)
+    #     return request.user.has_perm("%s.%s" % (opts.app_label, codename))
 
-    def has_delete_permission(self, request, obj=None):
-        """
-        Return True if the given request has permission to change the given
-        Django model instance, the default implementation doesn't examine the
-        `obj` parameter.
-        Can be overridden by the user in subclasses. In such case it should
-        return True if the given request has permission to delete the `obj`
-        model instance. If `obj` is None, this should return True if the given
-        request has permission to delete *any* object of the given type.
-        """
-        opts = self.opts
-        codename = get_permission_codename("delete", opts)
-        return request.user.has_perm("%s.%s" % (opts.app_label, codename))
+    # def has_delete_permission(self, request, obj=None):
+    #     """
+    #     Return True if the given request has permission to change the given
+    #     Django model instance, the default implementation doesn't examine the
+    #     `obj` parameter.
+    #     Can be overridden by the user in subclasses. In such case it should
+    #     return True if the given request has permission to delete the `obj`
+    #     model instance. If `obj` is None, this should return True if the given
+    #     request has permission to delete *any* object of the given type.
+    #     """
+    #     opts = self.opts
+    #     codename = get_permission_codename("delete", opts)
+    #     return request.user.has_perm("%s.%s" % (opts.app_label, codename))
 
-    def has_view_permission(self, request, obj=None):
-        """
-        Return True if the given request has permission to view the given
-        Django model instance. The default implementation doesn't examine the
-        `obj` parameter.
-        If overridden by the user in subclasses, it should return True if the
-        given request has permission to view the `obj` model instance. If `obj`
-        is None, it should return True if the request has permission to view
-        any object of the given type.
-        """
-        opts = self.opts
-        codename_view = get_permission_codename("view", opts)
-        codename_change = get_permission_codename("change", opts)
-        return request.user.has_perm(
-            "%s.%s" % (opts.app_label, codename_view)
-        ) or request.user.has_perm("%s.%s" % (opts.app_label, codename_change))
+    # def has_view_permission(self, request, obj=None):
+    #     """
+    #     Return True if the given request has permission to view the given
+    #     Django model instance. The default implementation doesn't examine the
+    #     `obj` parameter.
+    #     If overridden by the user in subclasses, it should return True if the
+    #     given request has permission to view the `obj` model instance. If `obj`
+    #     is None, it should return True if the request has permission to view
+    #     any object of the given type.
+    #     """
+    #     opts = self.opts
+    #     codename_view = get_permission_codename("view", opts)
+    #     codename_change = get_permission_codename("change", opts)
+    #     return request.user.has_perm(
+    #         "%s.%s" % (opts.app_label, codename_view)
+    #     ) or request.user.has_perm("%s.%s" % (opts.app_label, codename_change))
 
 
 admin.site.register(Client, AdminClient)
--- events/models.py
+++ events/models.py
@@ -5,6 +5,8 @@ from django.contrib import admin
 from django.urls import reverse
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import get_user_model
+
 
 
 phone_validator = RegexValidator(
@@ -14,9 +16,10 @@ phone_validator = RegexValidator(
 
 class Assignment(models.Model):
     employee = models.ForeignKey(
-        'authentication.Employee', on_delete=models.CASCADE, null=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_revoked = models.DateTimeField(null=True, blank=True)
+        'authentication.Employee', on_delete=models.CASCADE, null=True,
+        blank=True, verbose_name="Employee assigned")
+    date_created = models.DateField(auto_now_add=True)
+    date_revoked = models.DateField(null=True, blank=True)
 
     class Meta:
         abstract = True
@@ -30,20 +33,26 @@ class TimeStamp(models.Model):
     class Meta:
         abstract = True
 
+    def assigned(self):
+        return self.assignment.employee
+
 
 class EmployeeClient(Assignment):
-    client = models.ForeignKey(
-        'events.Client', on_delete=models.CASCADE, null=True)
+    client = models.OneToOneField(
+        'events.Client', on_delete=models.CASCADE, null=True, blank=True,
+        related_name="assignment")
 
 
 class EmployeeContract(Assignment):
-    client = models.ForeignKey(
-        'events.Contract', on_delete=models.CASCADE, null=True)
+    client = models.OneToOneField(
+        'events.Contract', on_delete=models.CASCADE, null=True, blank=True,
+        related_name="assignment")
 
 
 class EmployeeEvent(Assignment):
-    client = models.ForeignKey(
-        'events.Event', on_delete=models.CASCADE, null=True)
+    client = models.OneToOneField(
+        'events.Event', on_delete=models.CASCADE, null=True, blank=True,
+        related_name="assignment")
 
 
 class Client(TimeStamp):
@@ -61,9 +70,6 @@ class Client(TimeStamp):
     adress = models.CharField(max_length=255, null=True, blank=True)
     city = models.CharField(max_length=100, null=True, blank=True)
     country = models.CharField(max_length=80, null=True, blank=True)
-    assignee = models.ManyToManyField(
-        'authentication.Employee', through=EmployeeClient,
-        related_name="clients")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -72,9 +78,6 @@ class Client(TimeStamp):
 class Contract(TimeStamp):
     client = models.ForeignKey(
         Client, on_delete=models.CASCADE, related_name="contracts")
-    assignee = models.ManyToManyField(
-        'authentication.Employee', through=EmployeeContract,
-        related_name="contracts")
     signed = models.BooleanField(default=False)
     amount_in_euros = models.FloatField(default=0)
     payment_date = models.DateField(null=True, blank=True)
@@ -83,11 +86,6 @@ class Contract(TimeStamp):
     def __str__(self):
         return f"{self.client} ({self.date_created.date()})"
 
-    @admin.display(description='show client')
-    def link_client(self):
-        link = reverse("admin:events_client_change", args=[self.client.id])
-        return (f'<a href="{link}">show contract</a>')
-
 
 class Event(TimeStamp):
     name = models.CharField(max_length=100)
@@ -95,9 +93,6 @@ class Event(TimeStamp):
         Contract, null=True, blank=True, on_delete=models.CASCADE,
         related_name="events")
     date = models.DateField(null=True, blank=True)
-    assignee = models.ManyToManyField(
-        'authentication.Employee', through=EmployeeEvent,
-        related_name="events")
 
     def __str__(self):
         return f"{self.name}"
